919e0321796b6c382b502de2cb869fe89c6091df
Matt Dean <mdean400@gmail.com>
2025-03-18 21:40:37 -0400

    simple arrow to go back to tournaments

--- src/app/admin/tournaments/[tournamentId]/_/tournament-template.tsx
+++ src/app/admin/tournaments/[tournamentId]/_/tournament-template.tsx
@@ -1,4 +1,5 @@
 import { eq } from "drizzle-orm";
+import { ArrowLeftIcon } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { db } from "~/db";
@@ -19,17 +20,20 @@ export async function TournamentTemplate({
       {/* Floating Navigation Bar */}
       <div className="sticky top-5 z-10">
         <div className="justify-between max-w-[900px] items-center mx-auto bg-sky-green-light shadow-md p-4 rounded-lg flex flex-wrap gap-4 border border-gray-200">
-          <h1 className="text-center whitespace-nowrap text-lg font-bold text-sky-700">
-            Tournament {tournament.name}
-          </h1>
-
-          <div className="flex justify-center gap-3">
+          <div className="flex items-center gap-x-2">
             <Link
               href={"/admin"}
-              className="px-4 py-2 bg-black text-white rounded-md transition-colors hover:from-[var(--green-700)] hover:to-[var(--green-700)]"
+              className="flex items-center justify-center bg-sky-700 text-white size-8 rounded-md transition-colors hover:bg-sky-800"
             >
-              Tournaments
+              <ArrowLeftIcon className="size-6" />
             </Link>
+
+            <h1 className="text-center whitespace-nowrap text-lg font-bold text-sky-700">
+              Tournament {tournament.name}
+            </h1>
+          </div>
+
+          <div className="flex justify-center gap-3">
             <Link
               href={`/admin/tournaments/${tournamentId}/checkins`}
               className="px-4 py-2 bg-green-gradient text-white rounded-md transition-colors hover:from-[var(--green-700)] hover:to-[var(--green-700)]"
