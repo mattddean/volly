6437e0a8d677c43a0fa440e2b08a868141d7c9ba
Matt Dean <mdean400@gmail.com>
2025-05-26 21:28:11 -0400

    fix helpers type errors

--- src/echo/client/helpers.ts
+++ src/echo/client/helpers.ts
@@ -9,12 +9,17 @@ export function createClientHelpers<TSchema extends TSchemaType>(
   return {
     useOperation: <TInputSchema extends StandardSchemaV1, TOutput>(
       input: Parameters<typeof useOperation<TInputSchema, TOutput, TSchema>>[0],
+      options?: Parameters<
+        typeof useOperation<TInputSchema, TOutput, TSchema>
+      >[3],
     ) => {
       const { clientCtx, syncClient } = useEchoContext();
       return useOperation<TInputSchema, TOutput, TSchema>(
         input,
         syncClient,
-        clientCtx,
+        // TODO: remove type assertion
+        clientCtx as any,
+        options,
       );
     },
   };
